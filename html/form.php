e4229144284052332a3e0550c4acec9d282672f2
nobelai <fayaz.golsangi@gmail.com>
2019-01-15 17:18:00 +0530

    bug fixes

--- html/form.php
+++ html/form.php
@@ -1,6 +1,18 @@
 <?php
 //index.php
 
+ob_start();
+session_start();
+require_once 'dbconnect.php';
+
+if (!isset($_SESSION['user'])) {
+    header("Location: login.php");
+    exit;
+}
+// select logged in users detail
+$res = $conn->query("SELECT * FROM users WHERE id=" . $_SESSION['user']);
+$userRow = mysqli_fetch_array($res, MYSQLI_ASSOC);
+
 $error = '';
 $fname = '';
 $lname = '';
@@ -100,6 +112,45 @@ if(isset($_POST["submit"]))
  </head>
  <body>
   <br />
+ 
+ <!-- Navigation Bar-->
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                    aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">Lead Generation Tool</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="index.php">Home</a></li>
+                <li class="active"><a href="form.php">Leads</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                       aria-expanded="false">
+                        <span
+                            class="glyphicon glyphicon-user"></span>&nbsp;Logged
+                        in: <?php echo $userRow['email']; ?>
+                        &nbsp;<span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="logout.php?logout"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Logout</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<br/>
+<br/>
   <div class="container">
    <div class="col-md-6" style="margin:0 auto; float:none;">
     <form method="post">
